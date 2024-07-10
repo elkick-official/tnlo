e37ce58bf73831174f9ca482969d1863a9522572
venkateshprasad598 <venkateshprasad598@gmail.com>
2024-07-11 00:51:37 +0530

    Set up done tn project

---
+++ src/App.tsx
@@ -0,0 +1,34 @@
+import { ConfigProvider } from "antd";
+import { Suspense } from "react";
+import { Route, BrowserRouter as Router, Routes } from "react-router-dom";
+import "./App.css";
+import Layout from "./layout/Layout";
+import { routeList, routeListProps } from "./routes";
+import { configProvider, lightTheme } from "./utils";
+
+function App() {
+  return (
+    <>
+      <Suspense fallback={<>Loading</>}>
+        <ConfigProvider {...configProvider} theme={lightTheme}>
+          <Router>
+            <Routes>
+              <Route path="/" element={<h1>Login</h1>} />
+              <Route element={<Layout />}>
+                {routeList.map((route: routeListProps) => (
+                  <Route
+                    key={route.id}
+                    path={route.to}
+                    element={route.element}
+                  />
+                ))}
+              </Route>
+            </Routes>
+          </Router>
+        </ConfigProvider>
+      </Suspense>
+    </>
+  );
+}
+
+export default App;
