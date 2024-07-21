f3848f57f8a71baa83283bc41d949966085fe415
venkateshprasad598 <venkateshprasad598@gmail.com>
2024-07-22 00:41:02 +0530

    project set up done

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,17 @@
 import { ConfigProvider } from "antd";
-import { Suspense } from "react";
+import React, { Suspense } from "react";
 import { Route, BrowserRouter as Router, Routes } from "react-router-dom";
 import "./App.css";
 import Layout from "./layout/Layout";
 import { routeList, routeListProps } from "./routes";
 import { configProvider, lightTheme } from "./utils";
 
+const RemoteButton = React.lazy(() => import("remoteApp/Button"));
+
 function App() {
   return (
     <>
+      <RemoteButton />
       <Suspense fallback={<>Loading</>}>
         <ConfigProvider {...configProvider} theme={lightTheme}>
           <Router>
