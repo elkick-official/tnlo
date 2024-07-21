178a6cec3ec607f0240fa0e2716c9f86e73bfd92
venkateshprasad598 <venkateshprasad598@gmail.com>
2024-07-22 01:13:01 +0530

    project setup completed

--- src/App.tsx
+++ src/App.tsx
@@ -5,31 +5,31 @@ import "./App.css";
 import Layout from "./layout/Layout";
 import { routeList, routeListProps } from "./routes";
 import { configProvider, lightTheme } from "./utils";
-
-const RemoteButton = React.lazy(() => import("remoteApp/Button"));
+import { ErrorBoundary } from "./components/ErrorBoundary";
 
 function App() {
   return (
     <>
-      <RemoteButton />
-      <Suspense fallback={<>Loading</>}>
-        <ConfigProvider {...configProvider} theme={lightTheme}>
-          <Router>
-            <Routes>
-              <Route path="/" element={<h1>Login</h1>} />
-              <Route element={<Layout />}>
-                {routeList.map((route: routeListProps) => (
-                  <Route
-                    key={route.id}
-                    path={route.to}
-                    element={route.element}
-                  />
-                ))}
-              </Route>
-            </Routes>
-          </Router>
-        </ConfigProvider>
-      </Suspense>
+      <ErrorBoundary>
+        <Suspense fallback={<>Loading</>}>
+          <ConfigProvider {...configProvider} theme={lightTheme}>
+            <Router>
+              <Routes>
+                <Route path="/" element={<h1>Login</h1>} />
+                <Route element={<Layout />}>
+                  {routeList.map((route: routeListProps) => (
+                    <Route
+                      key={route.id}
+                      path={route.to}
+                      element={route.element}
+                    />
+                  ))}
+                </Route>
+              </Routes>
+            </Router>
+          </ConfigProvider>
+        </Suspense>
+      </ErrorBoundary>
     </>
   );
 }
