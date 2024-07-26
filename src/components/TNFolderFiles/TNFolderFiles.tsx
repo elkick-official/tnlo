b82e0d9bea86a995a37c4c228e66034736219e04
Viram Goyani <vaghasiyab347@gmail.com>
2024-07-27 02:00:42 +0530

    legency page done

--- src/components/TNFolderFiles/TNFolderFiles.tsx
+++ src/components/TNFolderFiles/TNFolderFiles.tsx
@@ -1,32 +1,37 @@
+import React from "react";
 import { noticeFile } from "../../const/imageData";
+import { dataLegencyFolder } from "../../const/data";
 import "./TNFolderFiles.css";
 
-export const TNFolderFiles = () => {
+  export const TNFolderFiles = () => {
     return(
         <>
             <div className="mt-3">
                 <h3 className="pb-3 fw-500">Folders</h3>
                 <hr className="opacity-03"/>
                 <div className="data-legency-folder-view grid mt-5">
-                        <div className="data-legency-folder-box flex justify-end flex-col active" style={{backgroundColor: "#6985E2"}}>
-                            <h4 className="fw-500 h5">Homicide</h4>
-                            <h6 className="fw-500 h6 mb-0">25 files</h6>
-                        </div>
-                        <div className="data-legency-folder-box flex justify-end flex-col" style={{backgroundColor: "#DAE0F9"}}>
-                            <h4 className="fw-500 h5">Homicide</h4>
-                            <h6 className="fw-500 h6 mb-0">25 files</h6>
-                        </div>
+                    {dataLegencyFolder.map((dlFolderItem, dlFolderIndex) => {
+                        return(
+                            <>
+                                <div  className="data-legency-folder-box flex justify-end flex-col transition-smooth" key={dlFolderIndex}
+                                >
+                                    <h4 className="fw-500 h5">{dlFolderItem.name}</h4>
+                                    <h6 className="fw-500 h6 mb-0">{dlFolderItem.files}</h6>
+                                </div>
+                            </>
+                        )
+                    })}
                 </div>
             </div>
             <div className="mt-7">
                 <h3 className="pb-3 fw-500">Files</h3>
                 <hr className="opacity-03"/>
                 <div className="data-legency-files-view grid mt-5">
-                        <div className="data-legency-folder-box" style={{backgroundColor: "#DAE0F9"}}>
+                        <div className="data-legency-folder-box transition-smooth">
                             <h4 className="fw-500 h5 mb-3">File 001</h4>
                             <img src={noticeFile} alt="notice-pdf" className="img-fluid" />
                         </div>
-                        <div className="data-legency-folder-box" style={{backgroundColor: "#DAE0F9"}}>
+                        <div className="data-legency-folder-box transition-smooth">
                             <h4 className="fw-500 h5 mb-3">File 002</h4>
                             <img src={noticeFile} alt="notice-pdf" className="img-fluid" />
                         </div>
--- src/const/data.tsx
+++ src/const/data.tsx
@@ -8,3 +8,21 @@ export const legencyModifiedOptions = [
     { label: "Modify 2", value: "2" },
     { label: "Modify 3", value: "3" },
 ];
+export const dataLegencyFolder = [
+    {
+        name: "Homicide",
+        files: "25 files",
+    },
+    {
+        name: "Assault",
+        files: "16 files",
+    },
+    {
+        name: "Theft and Robbery",
+        files: "12 files",
+    },
+    {
+        name: "Drug Offenses",
+        files: "18 files",
+    },
+]
