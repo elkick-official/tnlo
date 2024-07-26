33dfbc59cd3476f9ded93763c7293cbbdd2e7427
venkateshprasad598 <venkateshprasad598@gmail.com>
2024-07-27 03:02:08 +0530

    side bar icon made 20px and button name changes

--- src/page/LegacyDataDigitilization/LegacyDataDigitilization.tsx
+++ src/page/LegacyDataDigitilization/LegacyDataDigitilization.tsx
@@ -8,7 +8,7 @@ import { legencyModifiedOptions, legencyTypeOptions } from "../../const/data";
 const LegacyDataDigitilization = () => {
   // button js start
   const handleButtonClick = () => {
-    console.log('Button clicked');
+    console.log("Button clicked");
   };
   // button js end
   // input js start
@@ -19,77 +19,81 @@ const LegacyDataDigitilization = () => {
   };
   // input js end
   // select option js start
-  const [selectedValue, setSelectedValue] = useState<string | number | undefined>(undefined);
+  const [selectedValue, setSelectedValue] = useState<
+    string | number | undefined
+  >(undefined);
 
   const handleSelectChange = (value: string | number) => {
     setSelectedValue(value);
   };
   // select option js end
-  return <div>
-        <TNHeader children="Legacy Data Digitization"/>
-        <section className="xx-space yy-space">
-          <div className="legency-data-digitization-head-wrap flex items-center">
-            <div>
-              <TNInput
-                id="legency-search"
-                datatestid="legency-search-testid"
-                type="text"
-                name="legencySearch"
-                value={inputValue}
-                label="Seacrh something"
-                placeholder="Type something"
-                ILInputLabelClass="mb-0 legency-data-seacrhbar-input"
-                handleChange={handleInputChange}
-                textAreaShow={false}
-                readOnly={false}
-                searchBarControl
-                errorMsg="This is an error message"
-              />
-            </div>
-            <div className="ml-auto flex items-center gap-6">
-                <TNButton
-                id="new-folder"
-                datatestid="new-folder-testid"
-                type="button"
-                ILBtnClass="additional-class"
-                handleChange={handleButtonClick}
-                disabled={false}
-              >
-                New Folder
-              </TNButton>
-              <TNButton
-                id="upload-folder"
-                datatestid="upload-folder-testid"
-                type="button"
-                ILBtnClass="additional-class"
-                handleChange={handleButtonClick}
-                disabled={false}
-              >
-                Upload Folder
-              </TNButton>
-            </div>
-          </div>
-          <div className="legency-data-top-space legency-data-select-box flex items-center gap-6">
-            <TNSelect
-               label="Select an type"
-               options={legencyTypeOptions}
-               value={selectedValue}
-               onChange={handleSelectChange}
-               placeholder="Please select"
-            />
-            <TNSelect
-               label="Select modified"
-               options={legencyModifiedOptions}
-               value={selectedValue}
-               onChange={handleSelectChange}
-               placeholder="Please select"
+  return (
+    <div>
+      <TNHeader children="Legacy Data Digitization" />
+      <section className="xx-space yy-space">
+        <div className="legency-data-digitization-head-wrap flex items-center">
+          <div>
+            <TNInput
+              id="legency-search"
+              datatestid="legency-search-testid"
+              type="text"
+              name="legencySearch"
+              value={inputValue}
+              label="Seacrh something"
+              placeholder="Type something"
+              ILInputLabelClass="mb-0 legency-data-seacrhbar-input"
+              handleChange={handleInputChange}
+              textAreaShow={false}
+              readOnly={false}
+              searchBarControl
+              errorMsg="This is an error message"
             />
           </div>
-          <div className="legency-data-tab-wrap bg-tnl-white">
-            <TNTab/>
+          <div className="ml-auto flex items-center gap-6">
+            <TNButton
+              id="new-folder"
+              datatestid="new-folder-testid"
+              type="button"
+              ILBtnClass="additional-class"
+              handleChange={handleButtonClick}
+              disabled={false}
+            >
+              Create Folder
+            </TNButton>
+            <TNButton
+              id="upload-folder"
+              datatestid="upload-folder-testid"
+              type="button"
+              ILBtnClass="additional-class"
+              handleChange={handleButtonClick}
+              disabled={false}
+            >
+              Upload File
+            </TNButton>
           </div>
-        </section>
-  </div>
-}
+        </div>
+        <div className="legency-data-top-space legency-data-select-box flex items-center gap-6">
+          <TNSelect
+            label="Select an type"
+            options={legencyTypeOptions}
+            value={selectedValue}
+            onChange={handleSelectChange}
+            placeholder="Please select"
+          />
+          <TNSelect
+            label="Select modified"
+            options={legencyModifiedOptions}
+            value={selectedValue}
+            onChange={handleSelectChange}
+            placeholder="Please select"
+          />
+        </div>
+        <div className="legency-data-tab-wrap bg-tnl-white">
+          <TNTab />
+        </div>
+      </section>
+    </div>
+  );
+};
 
 export default LegacyDataDigitilization;
