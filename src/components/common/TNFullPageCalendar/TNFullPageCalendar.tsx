79d1e4024a3e01a4891020634d0fe2d5c2ff8bce
Viram Goyani <vaghasiyab347@gmail.com>
2024-07-27 13:19:34 +0530

    done full page calendar

--- src/components/common/TNFullPageCalendar/TNFullPageCalendar.tsx
+++ src/components/common/TNFullPageCalendar/TNFullPageCalendar.tsx
@@ -1,7 +1,18 @@
-import styles from "./TNFullPageCalendar.module.css";
-
-export const TNFullPageCalendar = () => {
+import { Calendar } from "antd";
+import type { CalendarProps } from 'antd';
+import type { Dayjs } from 'dayjs';
+import { FC, ReactNode } from "react";
+import "./TNFullPageCalendar.css";
+interface TNFullPageCalendarProps {
+    fullscreen: boolean;
+}
+export const TNFullPageCalendar: FC<TNFullPageCalendarProps> = ({fullscreen, ...rest}) => {
+    const onPanelChange = (value: Dayjs, mode: CalendarProps<Dayjs>['mode']) => {
+        console.log(value.format('YYYY-MM-DD'), mode);
+      };
     return(
-        <>Full page calendar</>
+        <>
+              <Calendar fullscreen={fullscreen} onPanelChange={onPanelChange} {...rest}/>
+        </>
     )
 }
--- src/page/CalendarForecast/CalendarForecast.tsx
+++ src/page/CalendarForecast/CalendarForecast.tsx
@@ -1,11 +1,76 @@
-import { Breadcrumb } from "antd";
+import { Col, Row } from "antd";
+import { TNButton, TNFullPageCalendar, TNHeader, TNInput } from "../../components";
+import { useState } from "react";
+import { PlusCircleOutlined } from "@ant-design/icons";
 import "./CalendarForecast.css";
-import { TNHeader } from "../../components";
-
 const CalendarForecast = () => {
+    // button js start
+      const handleButtonClick = () => {
+        console.log("Button clicked");
+    };
+    // button js end
+    // input js start
+    const [inputValue, setInputValue] = useState<string>("");
+
+    const handleInputChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+        setInputValue(e.target.value);
+    };
+    // input js end
+ 
   return <div>
        <TNHeader children="Calendar Forecast"/>
-  </div>;
+       <section className="xx-space yy-space">
+          <div className="legency-data-digitization-head-wrap flex items-center">
+                <div>
+                    <TNInput
+                    id="calendar-search"
+                    datatestid="calendar-search-testid"
+                    type="text"
+                    name="calendarSearch"
+                    value={inputValue}
+                    label="Type something"
+                    placeholder="Type something"
+                    ILInputLabelClass="mb-0 data-repository-seacrhbar-input"
+                    handleChange={handleInputChange}
+                    textAreaShow={false}
+                    readOnly={false}
+                    searchBarControl
+                    errorMsg="This is an error message"
+                    />
+                </div>
+                <div className="ml-auto">
+                  <TNButton
+                    id="add-program"
+                    datatestid="add-program-testid"
+                    type="button"
+                    ILBtnClass="additional-class ml-auto"
+                    handleChange={handleButtonClick}
+                    disabled={false}
+                    >
+                      
+                        <PlusCircleOutlined className="w-auto width min-w-auto pa-0 me-2 height min-h-auto"/>
+                        Add Program
+                    </TNButton>
+                </div>
+            </div>
+            <Row gutter={30}>
+              <Col xs={6}>
+                <div className="legency-data-tab-wrap bg-tnl-white mt-3">
+                  <h3 className="pb-3 fw-500">Calander</h3>
+                  <hr className="opacity-03"/>
+                  <TNFullPageCalendar fullscreen={false}/>
+                </div>
+              </Col>
+              <Col xs={18}>
+                <div className="legency-data-tab-wrap bg-tnl-white mt-3">
+                  <h3 className="pb-3 fw-500">Time Picker</h3>
+                  <hr className="opacity-03"/>
+                  <TNFullPageCalendar fullscreen={true}/>
+                </div>
+              </Col>
+            </Row>
+        </section>
+  </div>
 };
 
 export default CalendarForecast;
