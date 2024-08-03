e2f684b3f45dd26545580e8a10963ca0c3eef1cc
Viram Goyani <vaghasiyab347@gmail.com>
2024-08-03 12:00:43 +0530

    added login and register page design and also I have completed responsive design width desktop, tablet and mobiel device

--- src/page/Authentication/Login/Login.tsx
+++ src/page/Authentication/Login/Login.tsx
@@ -3,6 +3,7 @@ import { TNButton, TNInput } from "../../../components";
 import { useState } from "react";
 import { Link, useNavigate } from "react-router-dom";
 import "./../Authentication.css";
+import { logo } from "../../../const/imageData";
 const Login = () => {
      // input js start
      const [inputValue, setInputValue] = useState<string>("");
@@ -19,12 +20,14 @@ const Login = () => {
     return(
         <>
             <div className="auth-section flex">
-                <div className="auth-left-col">
-                    <div className="auth-box-content">
-                        {/* <h1 className="big primary-title text-center">Welcome Back!</h1> */}
+                <div className="auth-box-col w-100 auth-left-col px-2 px-sm-5 flex">
+                    <div className="auth-box-content py-6 text-center">
+                        <img src={logo} className="auth-logo"/>
+                        <h2 className="big primary-title text-center white text-tnl-white pb-0">Welcome to Tamil Nadu <br/>Law and Order IMS</h2>
                     </div>
+                    <span className="d-block py-2 auth-powered-label text-tnl-white h5 text-center flex items-center justify-center gap-4">Powered by <Link to="https://tejis.ai/" className="bg-tnl-primary-rgb py-1 px-2 fw-700 white text-tnl-white">Tejis.ai</Link></span>
                 </div>
-                <div className="auth-right-col flex">
+                <div className="auth-box-col w-100 auth-right-col flex py-5 py-md-8">
                     <div className="auth-form-box">
                         <h1 className="big primary-title text-center">Welcome Back!</h1>
                         <h6 className="title-label text-center">Enter your email and password get started</h6>
