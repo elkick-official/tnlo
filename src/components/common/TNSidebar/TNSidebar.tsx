9a59b2605b7ece45df16ee2d0b466e0b87e46761
Viram Goyani <vaghasiyab347@gmail.com>
2024-08-06 22:52:25 +0530

    added icons

--- src/components/common/TNSidebar/TNSidebar.tsx
+++ src/components/common/TNSidebar/TNSidebar.tsx
@@ -41,37 +41,44 @@ const items: MenuItem[] = [
   getItem(
     <Link to="/">Dashboard</Link>,
     "1",
-    <AppstoreOutlined style={{ fontSize: "20px", color: "black" }} />
+    // <AppstoreOutlined style={{ fontSize: "20px", color: "black" }} />
+   <b className="text-tnl-rapunzel-silver">{Icons.dashboardIcon}</b>
   ),
   getItem(
     <Link to="/legacy-data-digitilization">Legacy Data Digitization</Link>,
     "2",
-    <DatabaseOutlined style={{ fontSize: "20px", color: "black" }} />
+    // <DatabaseOutlined style={{ fontSize: "20px", color: "black" }} />
+    <b className="text-tnl-rapunzel-silver">{Icons.dataIcon}</b>
   ),
   getItem(
     <Link to="/data-repository">Data Repository</Link>,
     "3",
-    <CodeSandboxOutlined style={{ fontSize: "20px", color: "black" }} />
+    // <CodeSandboxOutlined style={{ fontSize: "20px", color: "black" }} />
+    <b className="text-tnl-rapunzel-silver">{Icons.dataRepositoryIcon}</b>
   ),
   getItem(
     <Link to="/dynamic-forms">Dynamic Forms</Link>,
     "4",
-    <FormOutlined style={{ fontSize: "20px", color: "black" }} />
+    // <FormOutlined style={{ fontSize: "20px", color: "black" }} />
+    <b className="text-tnl-rapunzel-silver">{Icons.dynamicFormsIcon}</b>
   ),
   getItem(
     <Link to="/calendar-forecast">Calendar Forecast</Link>,
     "5",
-    <CalendarOutlined style={{ fontSize: "20px", color: "black" }} />
+    // <CalendarOutlined style={{ fontSize: "20px", color: "black" }} />
+    <b className="text-tnl-rapunzel-silver">{Icons.calendarIcon}</b>
   ),
   getItem(
     <Link to="/press-note">Press Note</Link>,
     "6",
-    <BookOutlined style={{ fontSize: "20px", color: "black" }} />
+    // <BookOutlined style={{ fontSize: "20px", color: "black" }} />
+    <b className="text-tnl-rapunzel-silver">{Icons.pressNoteIcon}</b>
   ),
   getItem(
     <Link to="/settings">Settings</Link>,
     "7",
-    <SettingOutlined style={{ fontSize: "20px", color: "black" }} />
+    // <SettingOutlined style={{ fontSize: "20px", color: "black" }} />
+    <b className="text-tnl-rapunzel-silver">{Icons.settingIcon}</b>
   ),
 ];
 export const TNSidebar = () => {
